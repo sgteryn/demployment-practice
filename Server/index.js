069f20f693e04f9bb7e53bc9ea469cca416d0829
sgteryn <sgteryn@gmail.com>
2022-07-29 02:53:13 -0400

    initial commit

---
+++ Server/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require('express')
+const path= require('path')
+
+const app = express()
+
+app.use(express.static("client"));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
